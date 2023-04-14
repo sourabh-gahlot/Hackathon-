c07c2bd6332be544b8b52ebb79a378126ab55eb2
Sourabh <sourabhgahlot876@gmail.com>
2023-04-14 11:29:17 +0530

    creating models schema for restaurant

---
+++ models/restaurant.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => {
+      return Date.now();
+    },
+  },
+  updatedAt: {
+    type: Date,
+    default: () => {
+      return Date.now();
+    },
+  },
+});
+
+const Restaurant = mongoose.model("restaurant",restaurantSchema)
+
+module.exports= Restaurant;
